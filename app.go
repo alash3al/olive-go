a97feb930a49fc5c629ca6702c2cecc203b3e0f6
Mohammed Al Ashaal <m7medalash3al@gmail.com>
2015-12-09 08:04:52 +0200

    Update app.go

--- app.go
+++ app.go
@@ -2,44 +2,44 @@
 	Olive "Go", an advanced web-server framework written in Go .  
 
 	```
-	package main
-
-	import ( "github.com/alash3al/olive-go"; "net/http" )
-
-	func main(){
-		// lets set our base "localhost.me"
-		// this app will only work on "localhost.me/*"
-		// By default the hostname is "*"
-		// so it will work on any hostname, but here
-		// we change it to 'localhost.me' which is a free service
-		// that routes all requests to your own localhost server on port 80,
-		// you can use regex strings too .
-		app := olive.NewApp().SetHostname("localhost.me")
-
-		// just like the main http library
-		// but we have more features such as "regex strings" .
-		// You can chain multiple handleFunc calls from this one easily .
-		app.HandleFunc(`/hello`, func(ctx *olive.Context){
-			ctx.WriteJSON(map[string]string{
-				"message": "hello world",
+		package main
+	
+		import ( "github.com/alash3al/olive-go"; "net/http" )
+	
+		func main(){
+			// lets set our base "localhost.me"
+			// this app will only work on "localhost.me/*"
+			// By default the hostname is "*"
+			// so it will work on any hostname, but here
+			// we change it to 'localhost.me' which is a free service
+			// that routes all requests to your own localhost server on port 80,
+			// you can use regex strings too .
+			app := olive.NewApp().SetHostname("localhost.me")
+	
+			// just like the main http library
+			// but we have more features such as "regex strings" .
+			// You can chain multiple handleFunc calls from this one easily .
+			app.HandleFunc(`/hello`, func(ctx *olive.Context){
+				ctx.WriteJSON(map[string]string{
+					"message": "hello world",
+				})
 			})
-		})
-
-			// creating sub apps (routes) is so easy .
-			api := app.NewSubApp().SetHostname(`api.localtest.me`)
-
-				// nested sub-apps
-				apiAuth := api.NewSubApp().SetPath(`/auth/?.*`)
-
-				// login
-				apiAuth.HandleFunc(`/login`, func(ctx *Context){
-					ctx.WriteJSON(map[string]string{
-						"message": "login",
-					})
-				}).SetMethod(`POST`)
-
-		app.ListenAndServe(":80")
-	}
+	
+				// creating sub apps (routes) is so easy .
+				api := app.NewSubApp().SetHostname(`api.localtest.me`)
+	
+					// nested sub-apps
+					apiAuth := api.NewSubApp().SetPath(`/auth/?.*`)
+	
+					// login
+					apiAuth.HandleFunc(`/login`, func(ctx *Context){
+						ctx.WriteJSON(map[string]string{
+							"message": "login",
+						})
+					}).SetMethod(`POST`)
+	
+			app.ListenAndServe(":80")
+		}
 	```
 */
 package olive
